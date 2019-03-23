ea129fa1733a8b0aedddc262d2eaab5ce2b3ac18
Aron Jon Prenovost <Aroniascoder@gmail.com>
2019-03-23 10:42:55 -0700

    stubbed out todo's

--- src/modules/compareData.js
+++ src/modules/compareData.js
@@ -1,6 +1,39 @@
-function compareData(arg1, arg2) {
-	let matchMap = {}; 
+
+
+function compareData(genData, arg2, sortType) {
+	/*
+	if (!genData || !arg2) { return false; }
+	if (!genData.length !== !arg2.length) { return false; }
+	*/
+
+	// sort arg2
+
+	// use 'sortType'
+
+	// compare, +1 opacity for matches
+
+	// repeat
+	
+	let sortedData = []; 
 	// to do... 
+
+	for (var i = 0; i < genData.length; i++) {
+	let cell = {}
+		if (i > 0) {
+			for (var c = 0; c < i; c++) {
+				if (sortedData[c].name === r[0]) {
+					cell.color = sortedData[c].color;
+				}
+			}
+			if (!cell.color) {
+				cell.color = randColor; 
+			}
+		} else {
+			cell.color = randColor;
+		}
+	}
+		sortedData.push(cell)
+		i++; 
 }
 
 export {compareData};
--- src/modules/generateData.js
+++ src/modules/generateData.js
@@ -3,38 +3,14 @@ const generateData = () => {
 	let i = 0; 
 	while (i < 3600) {
 		let item = {}
-		// let r = Math.random().toString(36).substring(7);
-		// item.name = r[0];
-
 		let r = i; // order 1, 2, 3... 
 		item.name = r;
 		item.color = '#3498db';
 		item.opacity = 1;  
-		
-		// generates color on blue spectrum
-		// var randColor = "rgb(0,100, " + (Math.floor(Math.random() * 675)) + ")";
-		// generate HSL
-		/*
-		var randColor = "hsl(" + 360 * Math.random() + ',' +
-		(25 + 70 * Math.random()) + '%,' + 
-		(85 + 10 * Math.random()) + '%)'
-		if (i > 0) {
-			for (var c = 0; c < i; c++) {
-				if (newDataSet[c].name === r[0]) {
-					item.color = newDataSet[c].color;
-				}
-			}
-			if (!item.color) {
-				item.color = randColor; 
-			}
-		} else {
-			item.color = randColor;
-		}
-		*/
 		newDataSet.push(item)
 		i++; 
 	}
-return newDataSet;  
+	return newDataSet;  
 } 
  
 export {generateData};
