6de61e91f304c1e136c0331ff886580e56ac546e
Aron Prenovost <Aroniascoder@gmail.com>
2019-04-03 19:46:20 -0700

    Update generateData.js

--- src/modules/generateData.js
+++ src/modules/generateData.js
@@ -5,7 +5,17 @@ const generateData = () => {
 		let item = {}
 		let r = i; // order 1, 2, 3... 
 		item.key = r;
-		item.color = '#3dc1d3';
+		item.baseColor = '#f6e58d';
+		item.color1 = '#ffbe76';
+		item.color2 = '#ff7979';
+		item.color3 = '#badc58';
+		item.color4 = '#dff9fb';
+		item.color5 = '#f9ca24';
+		item.color6 = '#f0932b';
+		item.color7 = '#eb4d4b';
+		item.color8 = '#6ab04c';
+		item.color9 = '#22a6b3';
+		item.color10 = '#be2edd';
 		item.opacity = .1;  
 		newDataSet.push(item)
 		i++; 
