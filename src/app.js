5ae5600d777544337b0a30d01c0ba8ba1baf6f0c
Aron Prenovost <Aroniascoder@gmail.com>
2019-04-03 19:52:36 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,15 +8,48 @@ generateTable();
 
 // pass that sorted outcome to the table
 const colorTable = args => {
-   	let colors = document.getElementsByClassName('color');
-    for(let i = 0; i < colors.length; i++){
-   		if (!args) {
-   			colors[i].style.backgroundColor = '#efefef';
+let colors = document.getElementsByClassName('color');
+	for(let i = 0; i < colors.length; i++){
+		// monotone heat map 
+		/* if (!args) {
+   			colors[i].style.backgroundColor = '#c7ecee';
    		} else {
-			colors[i].style.backgroundColor = args[i].color;
+			colors[i].style.backgroundColor = args[i].baseColor;
 			colors[i].style.opacity = args[i].opacity;
-   		}
-    }
+   		}*/
+
+		// color spectrum 
+		if (!args) {
+			colors[i].style.backgroundColor = '#22a6b3';
+		} else {
+			// round to 1 decimal point, convert to # 
+			args[i].opacity = Number((Math.round(args[i].opacity * 10) / 10).toFixed(1));
+
+			if (args[i].opacity < .0) {
+				colors[i].style.backgroundColor = 'white';
+			} else if (args[i].opacity < .1) {
+				colors[i].style.backgroundColor = args[i].color1;
+			} else if (args[i].opacity < .2) {
+				colors[i].style.backgroundColor = args[i].color2;
+			} else if (args[i].opacity < .3) {
+				colors[i].style.backgroundColor = args[i].color3;
+			} else if (args[i].opacity < .4) {
+				colors[i].style.backgroundColor = args[i].color4;
+			} else if(args[i].opacity < .5) {
+				colors[i].style.backgroundColor = args[i].color5;
+			}else if (args[i].opacity < .6) {
+				colors[i].style.backgroundColor = args[i].color6;
+			} else if(args[i].opacity < .7) {
+				colors[i].style.backgroundColor = args[i].color7;
+			} else if (args[i].opacity < .8) {
+				colors[i].style.backgroundColor = args[i].color8;
+			} else if (args[i].opacity < .9) {
+				colors[i].style.backgroundColor = args[i].color9;
+			}else {
+				colors[i].style.backgroundColor = args[i].color10;
+			}
+		}
+	}
 }
 
 // compare pre and post sorted data
